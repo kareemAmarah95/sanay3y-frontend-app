75a62d5ac176b7af77a8b5573749c307294db024
Kareem Amara <kareemamarah2356@gmail.com>
2023-10-01 00:12:20 +0300

    adding the side navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
+import AsideNavbar from "./AsideNavbar";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <AsideNavbar />
     </>
   );
 };
