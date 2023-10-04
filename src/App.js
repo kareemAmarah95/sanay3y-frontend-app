909ad7a6594e0f9f0f2d6f8b4e3a0ca0ed09d5db
boody125 <104263878+boody125@users.noreply.github.com>
2023-10-04 22:06:28 +0300

    Add files via upload
    added useState HandleSubmits and Post requests to the back-end in the login/register routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import "./App.css";
 import Register from "./components/Register";
 import Login from "./components/Login";
-import Home from "./components/Home";
+import Navbar from "./components/Navbar"
+import AsideNavbar from "./components/AsideNavbar"
+import {BrowserRouter as Router ,Routes,Route} from "react-router-dom"
+import axios from "axios";
+
+axios.defaults.baseURL= 'http://localhost:8080'
 
 function App() {
   return (
     <div className="App">
-      {/* <Register /> */}
-      {/* <Login /> */}
-      <Home />
+      <Router>
+        {/* <Register /> */}
+        <Navbar/>
+        <Routes>  
+          <Route path="/" element={<AsideNavbar/>}/>
+          <Route path="/Register" element={<Register />}/>
+          <Route path="/Login" element={<Login />}/>
+        </Routes>
+        
+        {/* <Login /> */}
+      </Router>
+      
     </div>
   );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+
+import {React, useState} from 'react'
+import axios from "axios"
 import "./Login.css";
 const Login = () => {
+  const [email, setEmail]=useState("")
+  const [password, setPassword]=useState("")
+  
+  const handleSubmit =async (event)=>{
+    event.preventDefault()
+    try {
+      await axios.post('/Login',{
+        email,
+        password
+      });
+      alert('Login Successfull !')
+    } catch (error) {
+      alert('Login Failed !')
+    }
+    
+  }
+
+
+
   return (
     <>
       {/* bg-gray-950 */}
@@ -13,17 +34,15 @@ const Login = () => {
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 alt="Your Company"
               />
-              {/* text-white */}
-              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight  text-black">
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white text-black">
                 Sign in to your account
               </h2>
             </div>
-            <form className="space-y-6" action="#" method="POST">
+            <form className="space-y-6" onSubmit={handleSubmit}>
               <div>
-                {/* text-white */}
                 <label
                   htmlFor="email"
-                  className="block text-sm font-medium leading-6  text-black text-left"
+                  className="block text-sm font-medium leading-6 text-white text-black text-left"
                 >
                   Email address
                 </label>
@@ -31,7 +50,9 @@ const Login = () => {
                   <input
                     id="email"
                     name="email"
+                    value={email}
                     type="email"
+                    onChange={(ev)=>{setEmail(ev.target.value)}}
                     autocomplete="email"
                     required
                     className="block w-full rounded-md border-0 py-1.5 pl-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -41,10 +62,9 @@ const Login = () => {
 
               <div>
                 <div className="flex items-center justify-between">
-                  {/* text-white */}
                   <label
                     htmlFor="password"
-                    className="block text-sm font-medium leading-6  text-black"
+                    className="block text-sm font-medium leading-6 text-white text-black"
                   >
                     Password
                   </label>
@@ -61,7 +81,9 @@ const Login = () => {
                   <input
                     id="password"
                     name="password"
+                    value={password}
                     type="password"
+                    onChange={(ev)=>{setPassword(ev.target.value)}}
                     autocomplete="current-password"
                     required
                     className="block w-full rounded-md border-0 py-1.5 pl-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -70,10 +92,9 @@ const Login = () => {
               </div>
 
               <div>
-                {/* text-white */}
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6  shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 text-white"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Sign in
                 </button>
